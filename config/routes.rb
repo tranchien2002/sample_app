fd52db6074ed83b8435a2268a2dcf0da63e25dc7
tranchien2002 <chie.tm2002@gmail.com>
2017-09-22 20:53:49 +0700

    fillin static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
+  get '/home', to: "static_pages#home"
+
+  get "/help", to: "static_pages#help"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

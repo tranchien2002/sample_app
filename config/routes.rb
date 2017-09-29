d90d3ce700d512ba4f88da1b94fbe68e5737d0dd
ChieT <chie.tm2002@gmail.com>
2017-09-29 16:53:20 +0700

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+
+
 Rails.application.routes.draw do
 
   get 'users/new'
@@ -9,6 +11,10 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
 
   get  '/signup',  to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users

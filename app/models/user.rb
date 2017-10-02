26887516956702d04e75467b4a62e8ffbef3ebd1
tranchien2002 <chie.tm2002@gmail.com>
2017-10-02 21:01:41 +0700

    advance-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,12 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(self.remember_digest).is_password?(remember_token)
   end
 end
